4298b3d7e680b459f6f2600d498c8bf6e8d4e3d5
Gage <gage4systems@gmail.com>
2026-06-09 13:37:27 -0600

    day 1 js fundamentals

---
+++ index.js
@@ -0,0 +1,26 @@
+const username ='Alice';
+let age = 25;
+console.log(username);
+console.log(age);
+ function sayHello() {
+    console.log("hello,user!");
+ }
+sayHello();
+const hobbies =["coding","gym","music"];
+for (let i = 0; i <hobbies.length; i++ ) {
+    console.log(hobbies[i]);
+}
+console.log("start");
+
+setTimeout(() =>{
+    console.log("This runs after 2 seconds");
+},  2000);
+console.log("End"); 
+let price = 6;
+
+if (price > 5){ 
+  console.log("Expensive");
+} else {
+  console.log("Cheap");}
+
+
