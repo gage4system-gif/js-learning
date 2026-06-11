5f7baf588c39aab9615714145abdb8795bfe21da
Gage <gage4systems@gmail.com>
2026-06-11 16:18:18 -0600

    day 3 events

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-const ulElements = document.querySelectorAll("ul");
-ulElements.forEach(ulElement => {
-  const firstChild =ulElement.firstElementChild;
-  firstChild.style.backgroundColor = "purple";
+const button = document.querySelector('button')
+
+button.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'purple'
 })
-const element =document.getElementById("desserts");
-const lastChild = element.lastElementChild;
-lastChild.style.backgroundColor ="blue"
-
-
-ulElements.forEach(ulElement =>{
-    const lastChild =ulElement.lastElementChild;
-    lastChild.style.backgroundColor ="yellow"
-})
-const element2=document.getElementById("blueberries");
-const nextSibling2 =element.nextElementSibling;
-nextSibling2.style.backgroundColor ="yellow"
-
-
-const element3 =document.getElmentById("gelato");
-const prevSibling =element3.previousElementSibling;
-prevSibling.style.backgroundColor ="yellow";
-
-
-
-const element4 =document.getElementById("gelato");
-const parent =element4.parentElement.parentElement;
-parent.style.backgroundColor ="blue"
-
-
-const element5 = document.getElementById("fruits");
-const children =element.children;
-console.log(children)
