7e0a6cf1d677b43e05648eec018aa7082f3cd635
Gage <gage4systems@gmail.com>
2026-06-11 17:39:52 -0600

    day 4-5 fetch API async await and node server

--- index.js
+++
@@ -1,7 +0,0 @@
-fetch('https://official-joke-api.appspot.com/random_joke')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.setup)
-    console.log(data.punchline)
-  })
-  .catch(error => console.log(error))
